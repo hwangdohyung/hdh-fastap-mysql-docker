76d34ccd104032e0dd5c13685b65b75465d31a32
hwangdohyung <107665717+hwangdohyung@users.noreply.github.com>
2022-08-26 09:17:44 +0900

    d

--- app/services/user.py
+++ app/services/user.py
@@ -5,6 +5,6 @@ class UserService(object):
     
     def login(self, id, password):
         loginip = User(id,password)
-        print(f'ID: {loginip.id}',f'PASSWORD: {loginip.password}')
+        print(f'ID: {loginip.id}',f'PASSWORD: {loginip.password}',f'로그인 성공')
         # print(f'PASSWORD: {loginip.password}')
         
