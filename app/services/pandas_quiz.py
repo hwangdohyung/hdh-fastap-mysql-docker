2acbea25430e75368e003ffe6ba61f7375b20ba8
hwangdohyung <107665717+hwangdohyung@users.noreply.github.com>
2022-08-29 15:48:26 +0900

    er

--- app/services/pandas_quiz.py
+++ app/services/pandas_quiz.py
@@ -1,6 +1,10 @@
-from random import random
+import random
 from icecream import ic
 import pandas as pd 
+import string
+from pyparsing import col
+import numpy as np 
+
 class PandasQuiz(object):
     def __init__(self) -> None:
         pass
@@ -18,53 +22,78 @@ class PandasQuiz(object):
         orient='index',
         columns=['A','B','C']) 
         ic(df)
-        
-    def quiz_03(self) -> None : 
-        df = pd.DataFrame.from_dict(
-        {'0':[95, 25, 74],'1':[44, 24, 97]},
-        orient = 'index',
-        columns=['0','1','2'])
-        dff = pd.DataFrame.sample(n=(2,3))
-        ic(dff)
-            
-        
-        
-        
-        
-        
-        #  '''Q3 두자리 정수를 랜덤으로 2행 3열 데이터프레임을 생성
-        # ic| df3:     0   1   2
-        #          0  95  25  74
-        #          1  44  24  97
-        #          '''
-        
-        
-        
-        
-        
-        
-    '''
-    Q1. 다음 결과 출력
-       a  b  c 
-    1  1  3  5
-    2  2  4  6
-    ic| df1:       a  b  c
-                1  1  3  5
-                2  2  4  6
-    '''
 
-'''         
-        Q2. 다음 결과 출력
-           A   B   C
-        1   1   2   3
-        2   4   5   6
-        3   7   8   9
-        4  10  11  12
-        ic| df2:     A   B   C
-                 1   1   2   3
-                 2   4   5   6
-                 3   7   8   9
-                 4  10  11  12
-        '''
+    def quiz_03(self) -> None :
+        random_1 = [random.randint(10, 99) for p in range(0, 3)]
+        random_2 = [random.randint(10, 99) for p in range(0, 3)]
+        df = pd.DataFrame.from_dict(
+            {'1' : random_1,'2' : random_2},
+            orient='index',
+            columns=['0', '1', '2'])
+        ic(df)
+    
+    def id(self):
+        return ["".join([random.choice(string.ascii_letters) for i in range(5)]) for i in range(10)]
+    
+    def score(self):
+        return np.random.randint(0, 100, (10, 4))
         
+    def quiz_04(self):
+        df = pd.DataFrame(self.score(),index= self.id(),columns=['국어','영어','수학','사회'])
+        ic(df)
+        return df
+    
+    ''' 
+    Q5 원하는 과목 점수만 출력하시오. (만약 국어라고 입력하면 아래와 같이 출력됨)
+        hVoGW    93
+        QkpKK    25
+        oDmky    82
+        qdTeX    51
+        XGzWk    34
+        PAwgj    85
+        vnTmB    28
+        wuxIm    58
+        AOQFG    32
+        jHChe    59
+        Name: 국어, dtype: int64
+    
+    '''
+    def quiz_05(self, subject) :
+        scores = self.quiz_04()
+        ic(scores)
+    ''' 
+    Q6 원하는 학생점수만 출력하시오. (아이디가 랜덤이므로 맨 위에 학생점수 출력으로 대체함)
+        lDZid  57  90  55  24
+    '''
+    def quiz_6(self, id) :
+        print(f'{id}의 성적출력') # 당연히 id 가 일치할리 없음. 형식적으로 출력함
+        scores = self.quiz_4()
         
+    '''
+    Q7 각 학생들의 점수의 총합과 마지막 행은 과목총점 추가해서 출력
+        ic| df5:  국어   영어   수학   사회   과학    총점
+                 hVoGW   93   44   14   94   86   331
+                 QkpKK   25   54   29   10    8   126
+                 oDmky   82   65   31   31    2   211
+                 qdTeX   51   56   56    3   13   179
+                 XGzWk   34   32   67   48   23   204
+                 PAwgj   85   24   16    8   22   155
+                 vnTmB   28   80   52   43   48   251
+                 wuxIm   58   94   93   54   83   382
+                 AOQFG   32   50   95    1   52   230
+                 jHChe   59   37   80   27   39   242
+                 과목총점   547  536  533  319  376  2311
+    '''
+    def quiz_7(self) :
+        scores = self.quiz_4() 
+    
+    
+    # 리스트로 DataFrame 생성하는 방법 (디폴트)
+    # df = pd.DataFrame([[],[],[],[]], index=[], columns=[])
+    
+    # 사전으로 DataFrame 생성하는 방법 (from_dict()사용) 
+    # df = pd.DataFrame.from_dict({}, orient='index', columns=[])
+    
+    
+    
+    
